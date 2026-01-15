c5d96cf7f7c7d6b420d17dd1658375f81e052969
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2026-01-14 16:18:46 -0800

    changesmade

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,29 +1,56 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
+using System;
 
 [TestClass]
-public class PriorityQueueTests
+public class PriorityQueue_Tests
 {
+    // TEST RESULTS:
+    // Confirms that items are added to the queue successfully.
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void Enqueue_AddsItemsCorrectly()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        PriorityQueue queue = new PriorityQueue();
+        queue.Enqueue("TaskOne", 1);
+        queue.Enqueue("TaskTwo", 2);
+
+        Assert.AreEqual("TaskTwo", queue.Dequeue());
     }
 
+    // TEST RESULTS:
+    // Verifies that the item with the highest priority is removed first.
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void Dequeue_RemovesHighestPriorityItem()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        PriorityQueue queue = new PriorityQueue();
+        queue.Enqueue("LowPriorityTask", 1);
+        queue.Enqueue("HighPriorityTask", 5);
+        queue.Enqueue("MediumPriorityTask", 3);
+
+        Assert.AreEqual("HighPriorityTask", queue.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    // TEST RESULTS:
+    // Ensures FIFO behavior when multiple items have the same priority.
+    [TestMethod]
+    public void Dequeue_FollowsFIFOWhenPrioritiesMatch()
+    {
+        PriorityQueue queue = new PriorityQueue();
+        queue.Enqueue("FirstTask", 2);
+        queue.Enqueue("SecondTask", 2);
+
+        Assert.AreEqual("FirstTask", queue.Dequeue());
+    }
+
+    // TEST RESULTS:
+    // Confirms the correct exception and message are thrown for an empty queue.
+    [TestMethod]
+    public void Dequeue_EmptyQueue_ThrowsException()
+    {
+        PriorityQueue queue = new PriorityQueue();
+
+        InvalidOperationException exception =
+            Assert.ThrowsException<InvalidOperationException>(() => queue.Dequeue());
+
+        Assert.AreEqual("The queue is empty.", exception.Message);
+    }
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -7,51 +7,41 @@
 /// less than they will stay in the queue forever.  If a person is out of turns then they will 
 /// not be added back into the queue.
 /// </summary>
+using System;
+using System.Collections.Generic;
+
 public class TakingTurnsQueue
 {
-    private readonly PersonQueue _people = new();
+  internal int Length;
+  private Queue<Person> _queue = new Queue<Person>();
 
-    public int Length => _people.Length;
-
-    /// <summary>
-    /// Add new people to the queue with a name and number of turns
-    /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
     public void AddPerson(string name, int turns)
     {
-        var person = new Person(name, turns);
-        _people.Enqueue(person);
+        _queue.Enqueue(new Person(name, turns));
     }
 
-    /// <summary>
-    /// Get the next person in the queue and return them. The person should
-    /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
-    /// if the queue is empty.
-    /// </summary>
     public Person GetNextPerson()
     {
-        if (_people.IsEmpty())
+        if (_queue.Count == 0)
         {
-            throw new InvalidOperationException("No one in the queue.");
+            throw new InvalidOperationException("The queue is empty.");
         }
-        else
+
+        Person person = _queue.Dequeue();
+
+        // Infinite turns (0 or less)
+        if (person.Turns <= 0)
         {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
-            {
-                person.Turns -= 1;
-                _people.Enqueue(person);
-            }
-
-            return person;
+            _queue.Enqueue(person);
         }
-    }
+        // Finite turns
+        else if (person.Turns > 1)
+        {
+            person.Turns--;
+            _queue.Enqueue(person);
+        }
+        // If Turns == 1 → do not re-add
 
-    public override string ToString()
-    {
-        return _people.ToString();
+        return person;
     }
 }
